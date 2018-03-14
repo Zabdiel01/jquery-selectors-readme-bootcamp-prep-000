88ef977e1cdd5795054666888e6aa77cf899aab0
Zabdiel Ramos <zabdiel.ramos01@gmail.com>
2018-03-14 06:12:12 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,8 +5,11 @@ function paragraphSelector() {
 function lastImageSelector() {
   return $('img:last')
 }
-function ninjaBabySelectro() {
+function ninjaBabySelector() {
   return $('#baby-ninja')
+}
+function divSelector(){
+ return $('.pics') 
 }
 function firstListItem() {
   return $('#pic-list li:first-child')
